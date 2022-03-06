568105bc33ad1e7ddf6e8b879996f0a605b00d37
Mayank Goyal <mayankgoyal.mait@gmail.com>
2022-03-06 23:29:39 +0530

    Assets added

---
+++ lib/core/utils/color_utils.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorUtils {
+  static const appColor = Color(0xffF86817);
+  static const textColor = Color(0xff000000);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+  static const shadowColor = Color(0x60000000);//38% opacity
+
+}
